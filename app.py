33bde2e8ea89aba5b569a528e73c7201cde6a55c
Syed Sabir <sabirsyed2005@gmail.com>
2025-04-23 10:38:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,45 @@ while n>0:
 if s==copy:
     print("strong")
 else:
-    print("weak")"""
+    print("weak")
+
+
+
+words = ['apple','kiwi','banana']
+upp = [word.upper() for word in words]
+print(upp)
+
+#nested list comprehensions on 2d matrix to flatten
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+flat = [num for row in matrix for num in row]
+print(flat)
+
+#using logical condition,replace all the -ve no's with zero's
+nums = [-2,-1,0,1,2]
+nonneg = [x if x>=0 else 0  for x in nums]
+print(nonneg)
+
+
+#finding index
+words = ['apple','kiwi','banana']
+fc = [word[3] for word in words]
+print(fc)
+
+#finding length
+words = ['apple','kiwi','banana']
+l = [len(word) for word in words]
+print(l)
+
+#filtering None values
+items = [1,None,2,None,3,4,5,]
+valid = [x for x in items if x is not None]
+print(valid)
+
+
+names = ['Vijay','Mark']
+ages = [45,69]
+pair = [(name,age) for name in names for age in ages]
+print(pair)"""
 
 
 
